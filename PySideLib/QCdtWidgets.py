a618acb9e28ab5a32899349ca29d5dfb25b0d9de
Takanori Kishikawa <auratus.lemma@gmail.com>
2020-04-18 11:19:04 +0900

    フォーマットをかけた

--- PySideLib/QCdtWidgets.py
+++ PySideLib/QCdtWidgets.py
@@ -31,20 +31,20 @@ class QTagWidget(QWidget):
 
         self.tags = []
         self.mainFrame = QFrame()
-        self.mainFrame.setStyleSheet('border:1px solid #76797C; border-radius: 1px;')
+        self.mainFrame.setStyleSheet(
+            'border:1px solid #76797C; border-radius: 1px;')
 
         self.mainLayout = QVBoxLayout()
-        self.mainLayout.setContentsMargins(0,0,0,0)
+        self.mainLayout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(self.mainLayout)
         self.mainLayout.addWidget(self.mainFrame)
 
         self.hLayout = QHBoxLayout()
         self.hLayout.setSpacing(4)
 
-
         self.lineEdit = QLineEdit()
         self.lineEdit.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Maximum)
-        
+
         completer = QPartialMatchCompleter(self.lineEdit)
         completer.setCompletionMode(QCompleter.PopupCompletion)
         self.lineEdit.setCompleter(completer)
@@ -56,20 +56,17 @@ class QTagWidget(QWidget):
         self.mainFrame.setLayout(self.hLayout)
 
         self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
-        self.hLayout.setContentsMargins(2,2,2,2)
-        
+        self.hLayout.setContentsMargins(2, 2, 2, 2)
+
         self.refresh()
 
         self.setup_ui()
 
-
     def show(self):
         self.show()
 
-
     def setup_ui(self):
         self.lineEdit.returnPressed.connect(self.create_tags)
-
 
     def create_tags(self):
         new_tags = self.lineEdit.text().split(', ')
@@ -78,7 +75,6 @@ class QTagWidget(QWidget):
         self.tags = list(set(self.tags))
         self.tags.sort(key=lambda x: x.lower())
         self.refresh()
-
 
     def refresh(self):
         for i in reversed(range(self.hLayout.count())):
@@ -93,10 +89,10 @@ class QTagWidget(QWidget):
             self.lineEdit.setDisabled(True)
             return
 
-
     def add_tag_to_bar(self, text):
         tag = QFrame()
-        tag.setStyleSheet('border:1px solid rgb(192, 192, 192); border-radius: 4px;')
+        tag.setStyleSheet(
+            'border:1px solid rgb(192, 192, 192); border-radius: 4px;')
         tag.setContentsMargins(2, 2, 2, 2)
         tag.setFixedHeight(28)
 
@@ -105,23 +101,22 @@ class QTagWidget(QWidget):
         hbox.setSpacing(10)
 
         tag.setLayout(hbox)
-        
+
         label = QLabel(text)
         label.setStyleSheet('border:0px')
         label.setFixedHeight(16)
         hbox.addWidget(label)
-        
+
         x_button = QPushButton('x')
         x_button.setFixedSize(20, 20)
         x_button.setStyleSheet('border:0px; font-weight:bold')
         x_button.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
         x_button.clicked.connect(partial(self.delete_tag, text))
         hbox.addWidget(x_button)
-        
+
         tag.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Preferred)
 
         self.hLayout.addWidget(tag)
-
 
     def delete_tag(self, tag_name):
         self.tags.remove(tag_name)
@@ -132,9 +127,8 @@ class QTagWidget(QWidget):
         self.refresh()
 
 
-
 class QPartialMatchCompleter(QCompleter):
-    
+
     def __init__(self, parent):
         super(QPartialMatchCompleter, self).__init__(parent)
 
@@ -147,11 +141,12 @@ class QPartialMatchCompleter(QCompleter):
 
     def updateModel(self):
         local_completion_prefix = self.local_completion_prefix
+
         class InnerProxyModel(QSortFilterProxyModel):
             def filterAcceptsRow(self, sourceRow, sourceParent):
                 index0 = self.sourceModel().index(sourceRow, 0, sourceParent)
                 return local_completion_prefix.lower() in self.sourceModel().data(index0).lower()
-                
+
         proxyModel = InnerProxyModel()
         proxyModel.setSourceModel(self.source_model)
         super(QPartialMatchCompleter, self).setModel(proxyModel)
